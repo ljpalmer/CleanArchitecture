2026fec74462720c3306a2feedd8fa9fa16dd1c4
Luis Palmer <108294729+ljpalmer@users.noreply.github.com>
2023-01-16 15:08:38 -0500

    New: Update & Delete Streamer Command

---
+++ CleanArchitecture.Application/Features/Streamers/Commands/DeleteStreamer/DeleteStreamerCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using CleanArchitecture.Application.Contracts.Infraestructure;
+using CleanArchitecture.Application.Contracts.Persistence;
+using CleanArchitecture.Application.Exceptions;
+using CleanArchitecture.Application.Features.Streamers.Commands.CreateStreamer;
+using CleanArchitecture.Domain;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.Features.Streamers.Commands.DeleteStreamer
+{
+    public class DeleteStreamerCommandHandler : IRequestHandler<DeleteStreamerCommand>
+    {
+        private readonly IStreamerRepository _streamerRepository;
+        private readonly IMapper _mapper;
+        private readonly ILogger<DeleteStreamerCommandHandler> _logger; //Trabajar sobre el objeto inyectado
+
+        public async Task<Unit> Handle(DeleteStreamerCommand request, CancellationToken cancellationToken)
+        {
+            var streamerToDelete = await _streamerRepository.GetByIdAsync(request.Id);
+            if (streamerToDelete == null)
+            {
+                _logger.LogError($"{request.Id} streamer no existe en el sistema");
+                throw new NotFoundException(nameof(Streamer), request.Id);
+            }
+            await _streamerRepository.DeleteAsync(streamerToDelete);
+            _logger.LogInformation($"El {request.Id} streamer fue eliminado con exito");
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ CleanArchitecture.Application/Features/Streamers/Commands/UpdateStreamer/UpdateStreamerCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using FluentValidation.Validators;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.Features.Streamers.Commands.UpdateStreamer
+{
+    public class UpdateStreamerCommandValidator : AbstractValidator<UpdateStreamerCommand>
+    {
+        public UpdateStreamerCommandValidator()
+        {
+            RuleFor(p => p.Nombre)
+                .NotNull().WithMessage("{Nombre} no permite valores nulos");
+            RuleFor(p => p.Url)
+                .NotNull().WithMessage("{Url}, no permite valores nulos");
+        }
+    }
+}
