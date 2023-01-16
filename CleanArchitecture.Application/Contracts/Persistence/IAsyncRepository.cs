23faa3380ef059f5a7dd3f64f4f40eaf54927bac
Luis Palmer <108294729+ljpalmer@users.noreply.github.com>
2023-01-16 10:35:43 -0500

    New: Interface Generica

---
+++ CleanArchitecture.Application/Contracts/Persistence/IAsyncRepository.cs
@@ -0,0 +1,23 @@
+﻿using CleanArchitecture.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.Contracts.Persistence
+{
+    public interface IAsyncRepository<T> where T : BaseDomainModel
+    {
+        Task<IReadOnlyList<T>> GetAllAsync();
+        Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate);
+
+        Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null,
+                                        Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+                                        string includeString = null,
+                                        bool disableTracking = true);
+
+    }
+}
