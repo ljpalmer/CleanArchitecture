43775e012b672df6e6274d10fc623a695df9b561
Luis Palmer <108294729+ljpalmer@users.noreply.github.com>
2023-01-16 15:41:27 -0500

    New: Behaviours with MediatR

---
+++ CleanArchitecture.Application/Behaviours/UnhandledExceptionBahaviour.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.Behaviours
+{
+    public class UnhandledExceptionBahaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : MediatR.IRequest<TResponse>
+    {
+        private readonly ILogger<TRequest> _logger;
+        public UnhandledExceptionBahaviour(ILogger<TRequest> logger){
+            _logger = logger;
+        }
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            try {
+                return await next();
+            }
+            catch (Exception ex) { 
+                var requestName = typeof(TRequest).Name;
+                _logger.LogError(ex, "Application Request: Sucedio una excepcion para el request {Name} {@Request}", requestName,request);
+                throw;
+            }
+        }
+    }
+}
